035afb22ab7ceb0fb2e88b39e93bdb9b8e8fd4f0
zahirbegulic@gmail.com <zahirbegulic@gmail.com>
2021-03-19 19:51:45 +0100

    Recursion

---
+++ ex01/src/index.js
@@ -0,0 +1,12 @@
+function myRecursion(arr, n){
+    if (n <= 1) {
+        return 1;
+    } else {
+        return myRecursion(arr, n-1) + arr[n-1];
+    };
+}
+
+console.log(myRecursion([1, 2, 3, 4], 2));
+console.log(myRecursion([1, 2, 3, 4], 3));
+console.log(myRecursion([1, 2, 3, 4, 5, 6], 5));
+module.exports = myRecursion;
